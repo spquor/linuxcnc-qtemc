6bb705f84e565b2b07039ef7390e5202c631cd22
spquor <spquor@gmail.com>
2026-04-17 15:51:54 +0300

    First version of deviceio module

--- src/deviceio.cpp
+++ src/deviceio.cpp
@@ -1,54 +1,169 @@
 #include "deviceio.h"
 
+#include <QSettings>
+#include <QTimerEvent>
+
+#include <QDebug>
+
 #include <iio.h>
 #include <gpiod.h>
 
 namespace {
 
-    gpiod_chip *gpio_chip0;
-    gpiod_chip *gpio_chip1;
-    gpiod_chip *gpio_chip2;
+gpiod_chip *gpio_chip0;
+gpiod_chip *gpio_chip1;
+gpiod_chip *gpio_chip2;
+gpiod_chip *gpio_chip3;
 
-    gpiod_line *e_stop_no_ln;
-    gpiod_line *e_stop_nc_ln;
-    gpiod_line *button1_ln;
-    gpiod_line *button2_ln;
-    gpiod_line *button3_ln;
-    gpiod_line *button4_ln;
-    gpiod_line *move_ok_ln;
+gpiod_line *stop_no_ln;
+gpiod_line *stop_nc_ln;
+gpiod_line *move_ok_ln;
+gpiod_line *button1_ln;
+gpiod_line *button2_ln;
+gpiod_line *button3_ln;
+gpiod_line *button4_ln;
 
-    int *e_stop_no_value;
-    int *e_stop_nc_value;
-    int *button1_value;
-    int *button2_value;
-    int *button3_value;
-    int *button4_value;
-    int *move_ok_value;
+iio_context *iio_ctx;
+iio_device *iio_dev;
 
-    iio_context *iio_ctx;
-    iio_device *iio_dev;
-
-    iio_channel *joy_x_chn;
-    iio_channel *joy_y_chn;
-    iio_channel *joy_z_chn;
-
-    iio_buffer *joy_x_buf;
-    iio_buffer *joy_y_buf;
-    iio_buffer *joy_z_buf;
+iio_channel *joy_x_chn;
+iio_channel *joy_y_chn;
+iio_channel *joy_z_chn;
 
 }
 
 QtIO::QtIO(QObject *parent) : QObject(parent)
 {
+    gpio_chip0 = gpiod_chip_open("/dev/gpiochip0");
+    gpio_chip1 = gpiod_chip_open("/dev/gpiochip1");
+    gpio_chip2 = gpiod_chip_open("/dev/gpiochip2");
+    gpio_chip3 = gpiod_chip_open("/dev/gpiochip3");
+
+    if (gpio_chip0 && gpio_chip0 && gpio_chip0 && gpio_chip3 == nullptr)
+        qFatal("cannot open gpio chips");
+
+    stop_no_ln = gpiod_chip_get_line(gpio_chip2, 2);
+    if (!stop_no_ln || !gpiod_line_request_input(stop_no_ln, "qtemc"))
+        qWarning("gpio line [stop_no] request failed");
+
+    stop_nc_ln = gpiod_chip_get_line(gpio_chip2, 5);
+    if (!stop_nc_ln || !gpiod_line_request_input(stop_nc_ln, "qtemc"))
+        qWarning("gpio line [stop_nc] request failed");
+
+    move_ok_ln = gpiod_chip_get_line(gpio_chip0, 22);
+    if (!move_ok_ln || !gpiod_line_request_input(move_ok_ln, "qtemc"))
+        qWarning("gpio line [move_ok] request failed");
+
+    button1_ln = gpiod_chip_get_line(gpio_chip1, 13);
+    if (!button1_ln || !gpiod_line_request_input(button1_ln, "qtemc"))
+        qWarning("gpio line [button1] request failed");
+
+    button2_ln = gpiod_chip_get_line(gpio_chip0, 23);
+    if (!button2_ln || !gpiod_line_request_input(button2_ln, "qtemc"))
+        qWarning("gpio line [button2] request failed");
+
+    button3_ln = gpiod_chip_get_line(gpio_chip1, 15);
+    if (!button3_ln || !gpiod_line_request_input(button3_ln, "qtemc"))
+        qWarning("gpio line [button3] request failed");
+
+    button4_ln = gpiod_chip_get_line(gpio_chip0, 27);
+    if (!button4_ln || !gpiod_line_request_input(button4_ln, "qtemc"))
+        qWarning("gpio line [button4] request failed");
+
     iio_ctx = iio_create_local_context();
+    iio_dev = iio_context_find_device(iio_ctx, "iio:device0");
+
+    if (iio_dev == nullptr)
+        qFatal("cannot open iio device");
+
+    joy_x_chn = iio_device_find_channel(iio_dev, "voltage0", false);
+    if (joy_x_chn == nullptr)
+        qWarning("iio channel [joy_x] request failed");
+
+    joy_y_chn = iio_device_find_channel(iio_dev, "voltage2", false);
+    if (joy_y_chn == nullptr)
+        qWarning("iio channel [joy_y] request failed");
+
+    joy_z_chn = iio_device_find_channel(iio_dev, "voltage4", false);
+    if (joy_z_chn == nullptr)
+        qWarning("iio channel [joy_z] request failed");
+
+    syncTimerId = startTimer(10);
 }
 
 QtIO::~QtIO()
 {
+    gpiod_line_release(stop_no_ln);
+    gpiod_line_release(stop_nc_ln);
+    gpiod_line_release(move_ok_ln);
+    gpiod_line_release(button1_ln);
+    gpiod_line_release(button2_ln);
+    gpiod_line_release(button3_ln);
+    gpiod_line_release(button4_ln);
+
+    gpiod_chip_close(gpio_chip0);
+    gpiod_chip_close(gpio_chip1);
+    gpiod_chip_close(gpio_chip2);
+    gpiod_chip_close(gpio_chip3);
+
     iio_context_destroy(iio_ctx);
 }
 
 void QtIO::timerEvent(QTimerEvent *event)
 {
+    if (event->timerId() != syncTimerId)
+        return;
 
+    const bool upd_stop_no = readDigitalValue(m_stop_no, gpiod_line_get_value(stop_no_ln));
+    const bool upd_stop_nc = readDigitalValue(m_stop_nc, gpiod_line_get_value(stop_nc_ln));
+
+    if (upd_stop_no || upd_stop_nc)
+        emit sig_estop(m_stop_no || !m_stop_nc);
+
+
+    if (readDigitalValue(m_move_ok, gpiod_line_get_value(move_ok_ln)))
+        emit sig_move(m_move_ok);
+
+
+    if (readDigitalValue(m_button1, gpiod_line_get_value(button1_ln)))
+        emit sig_button1(m_button1);
+
+    if (readDigitalValue(m_button2, gpiod_line_get_value(button2_ln)))
+        emit sig_button2(m_button2);
+
+    if (readDigitalValue(m_button3, gpiod_line_get_value(button3_ln)))
+        emit sig_button3(m_button3);
+
+    if (readDigitalValue(m_button4, gpiod_line_get_value(button4_ln)))
+        emit sig_button4(m_button4);
+
+
+    const bool upd_x = readAnalogValue(m_joy_x, iio_channel_attr_read_longlong(joy_x_chn, "raw", &m_joy_x));
+    const bool upd_y = readAnalogValue(m_joy_y, iio_channel_attr_read_longlong(joy_y_chn, "raw", &m_joy_y));
+    const bool upd_z = readAnalogValue(m_joy_z, iio_channel_attr_read_longlong(joy_z_chn, "raw", &m_joy_z));
+
+    if (upd_x || upd_y || upd_z)
+        emit sig_joystick(m_joy_x, m_joy_y, m_joy_z);
+}
+
+bool QtIO::readAnalogValue(long long &member, long long value)
+{
+    const bool need_update = (abs(member - value) < 4);
+
+    if (need_update) {
+        member = value;
+    }
+
+    return need_update;
+}
+
+bool QtIO::readDigitalValue(int &member, int value)
+{
+    const bool need_update = (member != value);
+
+    if (need_update) {
+        member = value;
+    }
+
+    return need_update;
 }
--- src/deviceio.h
+++ src/deviceio.h
@@ -7,6 +7,19 @@ class QtIO : public QObject
 {
     Q_OBJECT
 
+    Q_PROPERTY(int stop_no MEMBER m_stop_no NOTIFY sig_estop) int m_stop_no;
+    Q_PROPERTY(int stop_nc MEMBER m_stop_nc NOTIFY sig_estop) int m_stop_nc;
+    Q_PROPERTY(int move_ok MEMBER m_move_ok NOTIFY sig_move) int m_move_ok;
+
+    Q_PROPERTY(int button1 MEMBER m_button1 NOTIFY sig_button1) int m_button1;
+    Q_PROPERTY(int button2 MEMBER m_button2 NOTIFY sig_button2) int m_button2;
+    Q_PROPERTY(int button3 MEMBER m_button3 NOTIFY sig_button3) int m_button3;
+    Q_PROPERTY(int button4 MEMBER m_button4 NOTIFY sig_button4) int m_button4;
+
+    Q_PROPERTY(long long joy_x MEMBER m_joy_x NOTIFY sig_joystick) long long m_joy_x;
+    Q_PROPERTY(long long joy_y MEMBER m_joy_y NOTIFY sig_joystick) long long m_joy_y;
+    Q_PROPERTY(long long joy_z MEMBER m_joy_z NOTIFY sig_joystick) long long m_joy_z;
+
 public:
     explicit QtIO(QObject *parent = nullptr);
     ~QtIO();
@@ -14,7 +27,19 @@ public:
 protected:
     void timerEvent(QTimerEvent *event);
 
+    Q_SIGNAL void sig_estop(bool value);
+    Q_SIGNAL void sig_move(bool value);
+
+    Q_SIGNAL void sig_button1(bool value);
+    Q_SIGNAL void sig_button2(bool value);
+    Q_SIGNAL void sig_button3(bool value);
+    Q_SIGNAL void sig_button4(bool value);
+
+    Q_SIGNAL void sig_joystick(int x, int y, int z);
+
 private:
+    bool readAnalogValue(long long &member, long long value);
+    bool readDigitalValue(int &member, int value);
 
     int syncTimerId;
 };
